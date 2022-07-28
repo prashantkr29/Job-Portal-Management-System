14ca54e1e9a357eb64dbc55a79b2e0eac57a659f
prashantkr29 <prashant.k2901@gmail.com>
2022-07-28 22:48:19 +0530

    login page

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const loginpage=()=>{
     window.location.href='login.html';
 }
 
+let container = document.getElementById('container')
+
+toggle = () => {
+	container.classList.toggle('sign-in')
+	container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+	container.classList.add('sign-in')
+}, 200)
