92096691fbd7a0b5a67bd29140a07e6e412b4364
rithikrajyt <rrpandey8055@gmail.com>
2022-08-07 16:21:49 +0530

    Company Registration backend added

--- php/companyRegistration.php
+++ php/companyRegistration.php
@@ -1,14 +1,14 @@
 <?php
 require_once ('db.php');
 
-$CompanyID = $_POST['comp_id'];
-$CompanyName = $_POST['comp_name'];
-$ContactNumber = $_POST['cont_num'];
+$CompanyID = $_POST["comp_id"];
+$CompanyName = $_POST["comp_name"];
+$ContactNumber = $_POST["cont_num"];
 
 $sql = "INSERT INTO `company`(`companyid`, `name`, `contact_number`) VALUES ('$CompanyID','$CompanyName','$ContactNumber')";
 if ($conn->query($sql) === TRUE) {
     $script = "<script>
-    window.location.href='../dashboard.html';</script>";
+    window.location.href='../recruiter-register.html';</script>";
     echo $script;
     echo "Company registered sucessfully";
 } else {
